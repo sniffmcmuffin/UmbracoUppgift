75d9aab57580594a73dd6b90c2130b88d8d13a21
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-10-03 14:43:13 +0200

    Add project files.

---
+++ wwwroot/scripts/main.js
@@ -0,0 +1,14 @@
+function handleContactFormSubmit(e) {
+    e.preventDefault()
+
+    fetch('https://api.domain.com/contactform')
+    .then(() => {
+
+    })
+    .then(() => {
+
+    })
+    .catch(error => {
+
+    })
+}
